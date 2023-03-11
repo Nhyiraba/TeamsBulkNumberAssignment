67f582e6cff6a1fa51f6c9555cb5d565384668d8
Nhyiraba <nhyiraba@yahoo.com>
2023-03-11 14:10:52 +0800

    Notes update

--- PhoneNumberAssignment.ps1
+++ PhoneNumberAssignment.ps1
@@ -10,11 +10,10 @@
 
 <#
 .SYNOPSIS
-	Short description
+	Assingning number in bulk or single user
 .DESCRIPTION
-	Long description
-.PARAMETER Path
-	Specifies a path to one or more locations.
+	Assingning number in bulk or single user and results can be exported to csv file.
+	Export can seperate file for both failed and succeeded or all togther in a single file
 .PARAMETER BulkAssignment
 	this is a switch parameter, if specified then the BulkAssignmentCsvData must be provided, if not present single user 
 	action is performed
@@ -25,39 +24,31 @@
 	The default is none
 .PARAMETER BulkAssignmentCsvData
 	Path to CSV file for bulk number assignment. Mandatory if the BulkAssignment parameter is specified
-
 .EXAMPLE 
 	This will perform assignment for one user by first requesting the value for PhoneNumberType else it will failed
 	PS >
 	.\PhoneNumberAssignment.ps1
-
 .EXAMPLE 
 	This will perform assignment for one user
 	PS >
 	.\PhoneNumberAssignment.ps1 -PhoneNumberType DirectRouting
-
 .EXAMPLE
 	For bulk action specifing the source csv file directly
 	PS>
 	.\PhoneNumberAssignment.ps1 -PhoneNumberType DirectRouting -BulkAssignmentCsvData C:\Users\PNDT\Documents\GitHub\TeamsBulkNumberAssignment\NumebrAssignment.csv
-
 .EXAMPLE
 	For bulk action using the BulkAssignment switch, this will proceed to request the BulkAssignmentCsvData path
 	PS>
 	.\PhoneNumberAssignment.ps1 -PhoneNumberType DirectRouting -BulkAssignment
 
-.FUNCTIONALITY
-	Assigning of phone number to user(s)
-
 .OUTPUTS
 	Output from this cmdlet exports csv file which contains the results from your assign process
-.NOTES
+.LINK
 	if you have an error in running the script, you refer to the following link
 		https://social.technet.microsoft.com/wiki/contents/articles/38496.unblock-downloaded-powershell-scripts.aspx
 	for more information
 		https://learn.microsoft.com/en-us/powershell/module/teams/set-csphonenumberassignment?view=teams-ps
 		https://learn.microsoft.com/en-us/microsoftteams/assign-change-or-remove-a-phone-number-for-a-user
-
 #>
 
 [CmdletBinding(DefaultParameterSetName = "None")]
